b7968e64ac75dd697f7e89fcd20b55ed5c29c759
thoresonjd <thoresonjd@gmail.com>
2023-07-17 22:53:04 -0700

    Created mesh class

---
+++ include/learnopengl/mesh.h
@@ -0,0 +1,40 @@
+/**
+ * @file mesh.h
+ * @brief Mesh class declaration
+ * @date July 2023
+ * @see https://learnopengl.com/Model-Loading/Mesh
+ */
+
+#pragma once
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <vector>
+#include <learnopengl/shader_m.h>
+
+struct Vertex {
+	glm::vec3 position;
+	glm::vec3 normal;
+	glm::vec2 texCoords;
+};
+
+struct Texture {
+	unsigned int id;
+	std::string type;
+	std::string path;
+};
+
+class Mesh {
+public:
+	// mesh data
+	std::vector<Vertex> vertices;
+	std::vector<unsigned int> indices;
+	std::vector<Texture> textures;
+	Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures);
+	void draw(Shader& shader);
+private:
+	// render data
+	unsigned int vbo, vao, ebo;
+	void setupMesh();
+};
---
+++ lib/mesh.cpp
@@ -0,0 +1,60 @@
+/**
+ * @file mesh.cpp
+ * @brief Mesh class definition
+ * @date July 2023
+ * @see https://learnopengl.com/Model-Loading/Mesh
+ */
+
+#include <learnopengl/mesh.h>
+
+Mesh::Mesh(
+	std::vector<Vertex> vertices,
+	std::vector<unsigned int> indices,
+	std::vector<Texture> textures
+) : vertices(vertices),
+	indices(indices),
+	textures(textures) {
+	setupMesh();
+}
+
+void Mesh::draw(Shader& shader) {
+	unsigned int diffuseNr = 1;
+	unsigned int specularNr = 1;
+	for (unsigned int i = 0; i < textures.size(); i++) {
+		glActiveTexture(GL_TEXTURE0 + i);
+		std::string number;
+		std::string name = textures[i].type;
+		if (name == "texture_diffuse")
+			number = std::to_string(diffuseNr++);
+		else if (name == "texture_specular")
+			number = std::to_string(specularNr++);
+		shader.setInt((name + number).c_str(), i);
+		glBindTexture(GL_TEXTURE_2D, textures[i].id);
+	}
+	glBindVertexArray(vao);
+	glDrawElements(GL_TRIANGLES, static_cast<unsigned int>(indices.size()), GL_UNSIGNED_INT, 0);
+	glBindVertexArray(0);
+	// always good practice to set everything back to defaults once configured.
+	glActiveTexture(GL_TEXTURE0);
+}
+
+void Mesh::setupMesh() {
+	glGenVertexArrays(1, &vao);
+	glGenBuffers(1, &vbo);
+	glGenBuffers(1, &ebo);
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+	// vertex positions
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+	// vertex normals
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, normal));
+	// vertex texture coordinates
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, texCoords));
+	glBindVertexArray(0);
+}
