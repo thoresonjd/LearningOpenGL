df2131ff67a29fd1fe4d63ebe45d173b403fff7f
thoresonjd <thoresonjd@gmail.com>
2023-07-19 13:01:30 -0700

    Creating object borders with stencil testing

---
+++ src/4.advanced_opengl/2.stencil_testing/stencil_testing.cpp
@@ -0,0 +1,362 @@
+/**
+ * @file stencil_testing.cpp
+ * @brief Creating object borders with stencil testing
+ * @date July 2023
+ * @see https://learnopengl.com/Advanced-OpenGL/Depth-testing
+ */
+
+// OpenGL implementation
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+// GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+// Custom libs
+#include <learnopengl/shader_m.h>
+#include <learnopengl/camera.h>
+// Image loading
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+// C++ libs
+#include <iostream>
+
+/**
+ * Reads a provided texture into memory
+ * @param path - the path to the texture
+ * @param flipVertically - determines whether to flip the texture along the y-axis
+ * @return The a texture ID
+ */
+unsigned int loadTexture(const char* path, bool flipVertically = false);
+
+ /**
+  * Handle window resizing
+  * @param window - a GLFW window object
+  * @param width - new width of resize
+  * @param height - new height of resize
+  */
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+
+/**
+ * Handle mouse movement
+ * @param window - a GLFW window object
+ * @param xPos - x position of the mouse
+ * @param yPos - y position of the mouse
+ */
+void mouseCallback(GLFWwindow* window, double xPos, double yPos);
+
+/**
+ * Handle mouse scrolling
+ * @param window - a GLFW window
+ * @param xOffset - amount scrolled along the x-axis
+ * @param yOffset - amount scrolled along the y-axis
+ */
+void scrollCallback(GLFWwindow* window, double xOffset, double yOffset);
+
+/**
+ * Process keyboard input
+ * @param window - a GLFW window object
+ */
+void processInput(GLFWwindow* window);
+
+// OpenGL configurations
+const int OPENGL_VERSION_MAJOR = 3;
+const int OPENGL_VERSION_MINOR = 3;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const char* WINDOW_NAME = "Stencil testing";
+// shaders
+const char* VERT_SHADER = "src/4.advanced_opengl/2.stencil_testing/stencil_testing.vs";
+const char* FRAG_SHADER = "src/4.advanced_opengl/2.stencil_testing/stencil_testing.fs";
+const char* SINGLE_COLOR_FRAG_SHADER = "src/4.advanced_opengl/2.stencil_testing/stencil_testing_single_color.fs";
+// textures
+const char* CUBE_TEX = "assets/textures/marble.jpg";
+const char* PLANE_TEX = "assets/textures/metal.png";
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float aspectRatio = SCREEN_WIDTH / SCREEN_HEIGHT;
+float lastX = SCREEN_WIDTH / 2;
+float lastY = SCREEN_HEIGHT / 2;
+bool firstMouse = true;
+// timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+int main(void) {
+	// initialize GLFW and create window
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, OPENGL_VERSION_MAJOR);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, OPENGL_VERSION_MINOR);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_NAME, nullptr, nullptr);
+	if (!window) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+	glfwSetCursorPosCallback(window, mouseCallback);
+	glfwSetScrollCallback(window, scrollCallback);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	// initialize GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		glfwTerminate();
+		return EXIT_FAILURE;
+	}
+
+	// configure global OpenGL state
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_STENCIL_TEST);
+	glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE); // if depth and stencil tests fail, replace value in stencil buffer
+
+	// create shader program objects
+	Shader shader(VERT_SHADER, FRAG_SHADER);	
+	Shader singleColorShader(VERT_SHADER, SINGLE_COLOR_FRAG_SHADER);	
+
+	// establist vertices
+	float cubeVertices[] = {
+		// positions          // texture Coords
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+	};
+
+	float planeVertices[] = {
+		// positions          // texture Coords (note we set these higher than 1 (together with GL_REPEAT as texture wrapping mode). this will cause the floor texture to repeat)
+		 5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+		-5.0f, -0.5f,  5.0f,  0.0f, 0.0f,
+		-5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+
+		 5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+		-5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+		 5.0f, -0.5f, -5.0f,  2.0f, 2.0f
+	};
+
+	// cube VAO
+	unsigned int cubeVAO, cubeVBO;
+	glGenVertexArrays(1, &cubeVAO);
+	glGenBuffers(1, &cubeVBO);
+	glBindVertexArray(cubeVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), &cubeVertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glBindVertexArray(0);
+
+	// plane VAO
+	unsigned int planeVAO, planeVBO;
+	glGenVertexArrays(1, &planeVAO);
+	glGenBuffers(1, &planeVBO);
+	glBindVertexArray(planeVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), &planeVertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glBindVertexArray(0);
+
+	// load textures
+	unsigned int cubeTexture = loadTexture(CUBE_TEX);
+	unsigned int floorTexture = loadTexture(PLANE_TEX);
+
+	// activate shader before setting uniforms
+	shader.use();
+	shader.setInt("texture1", 0);
+
+	// render loop
+	while (!glfwWindowShouldClose(window)) {
+		// per-frame time logic
+		float currentFrame = static_cast<float>(glfwGetTime());
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		// process input
+		processInput(window);
+
+		// set color
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+		// enable depth testing
+		glEnable(GL_DEPTH_TEST);
+
+		// clear buffer bits so information does not overlap
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+		// model, view, projection matrices
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = camera.getViewMatrix();
+		glm::mat4 projection = glm::perspective(glm::radians(camera.getFOV()), aspectRatio, 0.1f, 100.0f);
+
+		// floor
+		glStencilMask(0x00);
+		shader.use();
+		glBindVertexArray(planeVAO);
+		glBindTexture(GL_TEXTURE_2D, floorTexture);
+		shader.setMat4("model", model);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glBindVertexArray(0);
+
+		// cubes
+		glStencilFunc(GL_ALWAYS, 1, 0xFF); // all fragments should pass the stencil test
+		glStencilMask(0xFF); // enable writing to the stencil buffer
+		glBindVertexArray(cubeVAO);
+		glActiveTexture(GL_TEXTURE0);	
+		glBindTexture(GL_TEXTURE_2D, cubeTexture);
+		shader.setMat4("view", view);
+		shader.setMat4("projection", projection);
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+		shader.setMat4("model", model);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
+		shader.setMat4("model", model);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		// upscaled cubes
+		glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
+		glStencilMask(0x00); // disable writing to stencil buffer
+		glDisable(GL_DEPTH_TEST);
+		singleColorShader.use();
+		glBindVertexArray(cubeVAO);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, cubeTexture);
+		singleColorShader.setMat4("view", view);
+		singleColorShader.setMat4("projection", projection);
+		float scale = 1.1f;
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
+		model = glm::scale(model, glm::vec3(scale, scale, scale));
+		singleColorShader.setMat4("model", model);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(scale, scale, scale));
+		singleColorShader.setMat4("model", model);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glStencilMask(0xFF);
+		glStencilFunc(GL_ALWAYS, 1, 0xFF);
+		glEnable(GL_DEPTH_TEST);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// optionally deallocate all resources
+	glDeleteVertexArrays(1, &cubeVAO);
+	glDeleteBuffers(1, &cubeVBO);
+	glDeleteVertexArrays(1, &planeVAO);
+	glDeleteBuffers(1, &planeVBO);
+
+	glfwTerminate();
+	return EXIT_SUCCESS;
+}
+
+unsigned int loadTexture(const char* path, bool flipVertically) {
+	unsigned int textureID;
+	glGenTextures(1, &textureID);
+	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(flipVertically);
+	unsigned char* data = stbi_load(path, &width, &height, &nrChannels, 0);
+	if (data) {
+		GLenum format;
+		if (nrChannels == 1)
+			format = GL_RED;
+		else if (nrChannels == 3)
+			format = GL_RGB;
+		else if (nrChannels == 4)
+			format = GL_RGBA;
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	}
+	else {
+		std::cout << "ERROR::TEXTURE::FAILED_TO_LOAD\n" << path << std::endl;
+	}
+	stbi_image_free(data);
+	return textureID;
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void mouseCallback(GLFWwindow* window, double xPos, double yPos) {
+	float xPosition = static_cast<float>(xPos);
+	float yPosition = static_cast<float>(yPos);
+	if (firstMouse) {
+		lastX = xPosition;
+		lastY = yPosition;
+		firstMouse = false;
+	}
+	float xOffset = xPosition - lastX;
+	float yOffset = lastY - yPosition; // reversed since y-coordinates range from top to bottom
+	lastX = xPosition;
+	lastY = yPosition;
+	camera.processMouseMovement(xOffset, yOffset);
+}
+
+void scrollCallback(GLFWwindow* window, double xOffset, double yOffset) {
+	camera.processMouseScroll(static_cast<float>(yOffset));
+}
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.processKeyboard(CameraMovement::FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.processKeyboard(CameraMovement::BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.processKeyboard(CameraMovement::LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.processKeyboard(CameraMovement::RIGHT, deltaTime);
+}
---
+++ src/4.advanced_opengl/2.stencil_testing/stencil_testing_single_color.fs
@@ -0,0 +1,5 @@
+#version 330 core
+out vec4 fragColor;
+void main() {
+	fragColor = vec4(0.04f, 0.28f, 0.26f, 1.0f);
+}
