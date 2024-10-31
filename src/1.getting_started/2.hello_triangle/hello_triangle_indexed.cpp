882ec18deb0cd4add713b2d2676550fad6bdfe39
thoresonjd <thoresonjd@gmail.com>
2024-10-30 20:49:37 -0700

    Hello, triangle! - Rendering a rectangle using indices

--- src/1.getting_started/2.hello_triangle/hello_triangle_indexed.cpp
+++ src/1.getting_started/2.hello_triangle/hello_triangle_indexed.cpp
@@ -1,7 +1,7 @@
 /**
  * @file hello_triangle_indexed.cpp
  * @brief Rendering a triangle with indices
- * @date June 2023
+ * @date Created: June 2023 | Last modified: October 2024
  * @see https://learnopengl.com/Getting-started/Hello-Triangle
  */
 
@@ -23,52 +23,60 @@ void framebufferSizeCallback(GLFWwindow* window, int width, int height);
  */
 void processInput(GLFWwindow* window);
 
-const int OPENGL_VERSION_MAJOR = 3;
-const int OPENGL_VERSION_MINOR = 3;
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
-const char* WINDOW_NAME = "Hello, triangle!";
+namespace {
+	// configurations
+	const int OPENGL_VERSION_MAJOR = 3;
+	const int OPENGL_VERSION_MINOR = 3;
+	const int SCREEN_WIDTH = 800;
+	const int SCREEN_HEIGHT = 600;
+	const char* WINDOW_NAME = "Hello, triangle!";
+	
+	const char* VERTEX_SHADER_SOURCE = R"(
+		#version 330 core
+		layout (location = 0) in vec3 aPos;
+		void main() {
+			gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+		}
+	)";
 
-const char* vertexShaderSource = R"(
-	#version 330 core
-	layout (location = 0) in vec3 aPos;
-	void main() {
-		gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);
-	}
-)";
-
-const char* fragmentShaderSource = R"(
-	#version 330 core
-	out vec4 fragColor;
-	void main() {
-		fragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
-	}
-)";
+	const char* FRAGMENT_SHADER_SOURCE = R"(
+		#version 330 core
+		out vec4 fragColor;
+		void main() {
+			fragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+		}
+	)";
+}
 
 int main(void) {
+	// initialize GLFW
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, OPENGL_VERSION_MAJOR);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, OPENGL_VERSION_MINOR);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// create GLFW window object
 	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_NAME, nullptr, nullptr);
 	if (!window) {
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
-		return EXIT_FAILURE;
+		return -1;
 	}
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+
+	// initialize GLAD: load OpenGL function pointers
+	if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		glfwTerminate();
-		return EXIT_FAILURE;
+		return -1;
 	}
 
 	// create vertex shader
 	int success;
 	char infoLog[512];
 	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+	glShaderSource(vertexShader, 1, &VERTEX_SHADER_SOURCE, nullptr);
 	glCompileShader(vertexShader);
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
 	if (!success) {
@@ -78,7 +86,7 @@ int main(void) {
 
 	// create fragment shader
 	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+	glShaderSource(fragmentShader, 1, &FRAGMENT_SHADER_SOURCE, nullptr);
 	glCompileShader(fragmentShader);
 	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
 	if (!success) {
@@ -86,7 +94,7 @@ int main(void) {
 		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
 	}
 
-	// link shaders and create shader program
+	// create shader program
 	unsigned int shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
@@ -94,71 +102,84 @@ int main(void) {
 	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
 	if (!success) {
 		glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
-		std::cout << "ERROR::PROGRAM::SHADER::LINKING_FAILED\n" << infoLog << std::endl;
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
 	}
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	// establist vertices and indices
+	// establish rectangle vertex data
 	float vertices[] = {
-		0.5f, 0.5f, 0.0f,   // top right
+		0.5f, 0.5f, 0.0f,	// top right
 		0.5f, -0.5f, 0.0f,  // bottom right
 		-0.5f, -0.5f, 0.0f, // bottom left
-		-0.5f, 0.5f, 0.0f,  // top left
+		-0.5f, 0.5f, 0.0f   // top left 
 	};
+
+	// establish rectangle index data
 	unsigned int indices[] = {
 		0, 1, 3, // first triangle
 		1, 2, 3  // second triangle
 	};
 
-	// create vertex buffer object, vertex array object, element buffer object
+	// create vertex buffer object, vertex array object, and element buffer object
 	unsigned int vbo, vao, ebo;
 	glGenVertexArrays(1, &vao);
 	glGenBuffers(1, &vbo);
 	glGenBuffers(1, &ebo);
+
+	// bind VAO before configuring vertex data so data is bound to VAO
 	glBindVertexArray(vao);
 
-	// copy vertices array into buffer
+	// copy vertices array into vertex buffer
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// copy index array elements into buffer
+	// copy indices into element buffer
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	// tell OpenGL how to interpret the vertex data via setting vertex attribute pointers
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), reinterpret_cast<void*>(0));
 	glEnableVertexAttribArray(0);
 
-	// unbind
+	// unbind VBO and VAO
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
-	// render in wireframe mode
-	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	// unbind EBO after VAO to keep EBO data bound to VAO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	
+	// enable wireframe mode
+#if 0
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+#endif
+
+	// render loop
 	while (!glfwWindowShouldClose(window)) {
+		// input
 		processInput(window);
+
+		// screen color
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// draw triangles
+		// draw triangle
 		glUseProgram(shaderProgram);
 		glBindVertexArray(vao);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
+		// swap buffers and poll IO events
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
-	// optionally deallocate all resources
+	// deallocate all resources
 	glDeleteVertexArrays(1, &vao);
 	glDeleteBuffers(1, &vbo);
 	glDeleteBuffers(1, &ebo);
 	glDeleteProgram(shaderProgram);
-
 	glfwTerminate();
-	return EXIT_SUCCESS;
+	return 0;
 }
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
