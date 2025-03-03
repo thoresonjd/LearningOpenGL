37246de3835cdd4fbdf25f55c025c0396be101b0
thoresonjd <thoresonjd@gmail.com>
2025-03-02 20:01:48 -0800

    Complete depth testing chapter

--- src/4.advanced_opengl/1.depth_testing/depth_testing/depth_testing.cpp
+++ src/4.advanced_opengl/1.depth_testing/depth_testing/depth_testing.cpp
@@ -1,24 +1,18 @@
 /**
  * @file depth_testing.cpp
  * @brief Experimenting with depth testing
- * @date July 2023
+ * @date Created: July 2023 | Last modified: March 2025
  * @see https://learnopengl.com/Advanced-OpenGL/Depth-testing
  */
 
-// OpenGL implementation
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-// GLM
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-// Custom libs
-#include <learnopengl/shader_m.h>
+#include <learnopengl/shader_g.h>
 #include <learnopengl/camera.h>
-// Image loading
-#define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
-// C++ libs
 #include <iostream>
 
 /**
@@ -59,27 +53,37 @@ void scrollCallback(GLFWwindow* window, double xOffset, double yOffset);
  */
 void processInput(GLFWwindow* window);
 
-// OpenGL configurations
-const int OPENGL_VERSION_MAJOR = 3;
-const int OPENGL_VERSION_MINOR = 3;
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
-const char* WINDOW_NAME = "Depth testing";
-// shaders
-const char* VERT_SHADER = "src/4.advanced_opengl/1.depth_testing/depth_testing/depth_testing.vs";
-const char* FRAG_SHADER = "src/4.advanced_opengl/1.depth_testing/depth_testing/depth_testing.fs";
-// textures
-const char* CUBE_TEX = "assets/textures/marble.jpg";
-const char* PLANE_TEX = "assets/textures/metal.png";
-// camera
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
-float aspectRatio = SCREEN_WIDTH / SCREEN_HEIGHT;
-float lastX = SCREEN_WIDTH / 2;
-float lastY = SCREEN_HEIGHT / 2;
-bool firstMouse = true;
-// timing
-float deltaTime = 0.0f;
-float lastFrame = 0.0f;
+namespace {
+	// GLFW
+	constexpr int OPENGL_VERSION_MAJOR = 3;
+	constexpr int OPENGL_VERSION_MINOR = 3;
+	constexpr int SCREEN_WIDTH = 800;
+	constexpr int SCREEN_HEIGHT = 600;
+	const char* WINDOW_NAME = "Depth Testing";
+
+	// Shaders
+	const char* VERT_SHADER = "src/4.advanced_opengl/1.depth_testing/depth_testing/depth_testing.vs";
+	const char* FRAG_SHADER = "src/4.advanced_opengl/1.depth_testing/depth_testing/depth_testing.fs";
+	
+	// Textures
+	const char* CUBE_TEX = "assets/textures/marble.jpg";
+	const char* PLANE_TEX = "assets/textures/metal.png";
+
+	// Camera
+	constexpr float ASPECT_RATIO = static_cast<float>(SCREEN_WIDTH) / static_cast<float>(SCREEN_WIDTH);
+	constexpr float NEAR_PLANE = 0.1f;
+	constexpr float FAR_PLANE = 100.0f;
+	Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+
+	// Time
+	float deltaTime = 0.0f;
+	float lastFrame = 0.0f;
+
+	// Mouse
+	bool isFirstMouse = true;
+	float lastMouseX = static_cast<float>(SCREEN_WIDTH) / 2.0f;
+	float lastMouseY = static_cast<float>(SCREEN_HEIGHT) / 2.0f;
+}
 
 int main(void) {
 	// initialize GLFW and create window
@@ -87,11 +91,13 @@ int main(void) {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, OPENGL_VERSION_MAJOR);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, OPENGL_VERSION_MINOR);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// create GLFW window object
 	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_NAME, nullptr, nullptr);
 	if (!window) {
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
-		return EXIT_FAILURE;
+		return -1;
 	}
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
@@ -99,11 +105,11 @@ int main(void) {
 	glfwSetScrollCallback(window, scrollCallback);
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-	// initialize GLAD
+	// initialize GLAD: load OpenGL function pointers
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		glfwTerminate();
-		return EXIT_FAILURE;
+		return -1;
 	}
 
 	// configure global OpenGL state
@@ -224,7 +230,7 @@ int main(void) {
 		shader.use();
 		glm::mat4 model = glm::mat4(1.0f);
 		glm::mat4 view = camera.getViewMatrix();
-		glm::mat4 projection = glm::perspective(glm::radians(camera.getFOV()), aspectRatio, 0.1f, 100.0f);
+		glm::mat4 projection = glm::perspective(glm::radians(camera.getFOV()), ASPECT_RATIO, NEAR_PLANE, FAR_PLANE);
 		shader.setMat4("view", view);
 		shader.setMat4("projection", projection);
 
@@ -259,7 +265,7 @@ int main(void) {
 	glDeleteBuffers(1, &planeVBO);
 
 	glfwTerminate();
-	return EXIT_SUCCESS;
+	return 0;
 }
 
 unsigned int loadTexture(const char* path, bool flipVertically) {
@@ -298,15 +304,15 @@ void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
 void mouseCallback(GLFWwindow* window, double xPos, double yPos) {
 	float xPosition = static_cast<float>(xPos);
 	float yPosition = static_cast<float>(yPos);
-	if (firstMouse) {
-		lastX = xPosition;
-		lastY = yPosition;
-		firstMouse = false;
+	if (isFirstMouse) {
+		lastMouseX = xPosition;
+		lastMouseY = yPosition;
+		isFirstMouse = false;
 	}
-	float xOffset = xPosition - lastX;
-	float yOffset = lastY - yPosition; // reversed since y-coordinates range from top to bottom
-	lastX = xPosition;
-	lastY = yPosition;
+	float xOffset = xPosition - lastMouseX;
+	float yOffset = lastMouseY - yPosition; // reversed since y-coordinates range from top to bottom
+	lastMouseX = xPosition;
+	lastMouseY = yPosition;
 	camera.processMouseMovement(xOffset, yOffset);
 }
 
@@ -315,14 +321,17 @@ void scrollCallback(GLFWwindow* window, double xOffset, double yOffset) {
 }
 
 void processInput(GLFWwindow* window) {
+	// window close / exit program
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+
+	// camera movement
 	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
 		camera.processKeyboard(CameraMovement::FORWARD, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		camera.processKeyboard(CameraMovement::BACKWARD, deltaTime);
 	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
 		camera.processKeyboard(CameraMovement::LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.processKeyboard(CameraMovement::BACKWARD, deltaTime);
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
 		camera.processKeyboard(CameraMovement::RIGHT, deltaTime);
 }
